01a70ceba26c6f5e61d8c9b9c8e17b1f49534e27
Rubiere Ramirez <64884499+Rubi3r3@users.noreply.github.com>
2023-12-01 10:52:47 -0600

    added a space before inserting table

--- R/scripts.r
+++ R/scripts.r
@@ -15,7 +15,7 @@ clear_ed<- function(c, e, t) {
     for (s in 1:length(e)) {
       e <- e[[s]]
  
-      query<- paste0("UPDATE",t," SET photovalid = NULL,
+      query<- paste0("UPDATE ",t," SET photovalid = NULL,
       photo_cnt = NULL, living_quarter = NULL, bldg_refn = bldg_number,
       bldg_newn = NULL,
       isbldg = NULL,
