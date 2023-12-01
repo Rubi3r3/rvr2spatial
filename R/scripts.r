57366c132faa1024136ea21e8243d45e54d48ab1
Rubiere Ramirez <64884499+Rubi3r3@users.noreply.github.com>
2023-12-01 11:00:19 -0600

    cleaned the code

--- R/scripts.r
+++ R/scripts.r
@@ -12,9 +12,8 @@
 clear_ed<- function(c, e, t) {
     conn2 <- c
     t <- t
-    for (s in 1:length(e)) {
-      e <- e[[s]]
- 
+    for (element in e) {
+       
       query<- paste0("UPDATE ",t," SET photovalid = NULL,
       photo_cnt = NULL, living_quarter = NULL, bldg_refn = bldg_number,
       bldg_newn = NULL,
@@ -22,7 +21,7 @@ clear_ed<- function(c, e, t) {
       population = NULL,
       tot_hh_number = NULL,
       interview__key = NULL,
-      int_bldg_desc = NULL WHERE ed_2023 = '", e,"'; ")
+      int_bldg_desc = NULL WHERE ed_2023 = '", element ,"'; ")
  
       DBI::dbExecute(conn2, query)
         
