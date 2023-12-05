c26668e2b7f25d96014cbd1f2bf75b7f5a8212ba
Rubiere Ramirez <64884499+Rubi3r3@users.noreply.github.com>
2023-12-05 15:48:11 -0600

    added a get_Vr function

--- R/scripts.r
+++ R/scripts.r
@@ -58,7 +58,7 @@ assign_interview__keys<- function (c, e) {
  
      DBI::dbExecute(c, query_clear)
 
-     query_fvr <- "SELECT * FROM fvr"
+     query_fvr <- "SELECT * FROM fullvr_mics"
      fvr<- dbGetQuery(db, query_fvr)
 
      if ("Orange Walk" %in% fvr$district) {
@@ -130,4 +130,90 @@ assign_interview__keys<- function (c, e) {
 
   }
 }
+
+#' Function to get visitation records and compare it to the spatail data.
+#'
+#'  
+#' @param imput ed
+#' 
+#' @param input location
+#'
+#' @returns the completed query, and a list of counts.
+#'
+#'@example
+#'get_vr('54-444-44', '\\directory\\')
+#' @export
+
+get_vr <- function(ed_list, location) {
+
+  ed_hhs<- data.frame()
+  raw_vrs<- data.frame()
+  vrs<- data.frame()
+  filter_vrs<- data.frame()
+  spat_building_process<- data.frame()
+  for(e in 1:length(ed_list)) {
+
+    ed_no <- ed_list[[e]]
+    
+    query_building<- "Select * from sde.mics7_building"
+    spat_building<- DBI::dbExecute (conn, query_building)
+
+    spat_building_filter <- subset(spat_building, ed_2023 == ed_no) %>%
+      st_drop_geometry()
+
+      query_fvr <- "SELECT * FROM fullvr_mics"
+     fvr<- dbGetQuery(db, query_fvr)
+
+    all_comp_vr_raw <- subset(fvr, ed == ed_no)
+
+    vr_inconsistencies_filter <- subset(vr_inconsistencies, ed == ed_no)
+
+    i_hhs<- all_comp_vr_raw %>%
+      group_by(interview__key, hhs_roster__id) %>%
+      tally()
+
+    i_hhs2<- na.omit(i_hhs)
+
+    i_hhs3<- aggregate(i_hhs2$n, list(i_hhs2$interview__key), FUN=sum)
+    names(i_hhs3)[names(i_hhs3) == 'Group.1'] <- 'interview__key'
+    names(i_hhs3)[names(i_hhs3) == 'x'] <- 'hhs_count'
+
+    total_hhs<- as.data.frame(sum(i_hhs3$hhs_count))
+
+    all_comp_vr_to_process <- all_comp_vr_raw %>%
+      distinct(interview__key, .keep_all = TRUE)
+    all_comp_vr_to_process <- filter(all_comp_vr_to_process, responsible1.x != 'DELETION_VR23')
+
+    filter_all_comp_vr_to_process <- filter(all_comp_vr_to_process, isBuilding == 'Yes - Main Building' | living_quarter == 'Yes')
+
+    ed_hhs<- rbind(ed_hhs, i_hhs3)
+    raw_vrs<- rbind(raw_vrs, all_comp_vr_raw)
+    vrs <- rbind(vrs,all_comp_vr_to_process)
+    filter_vrs<- rbind(filter_vrs,filter_all_comp_vr_to_process)
+    spat_building_process<-rbind(spat_building_process, spat_building_filter)
+
+    write.csv(filter_for_gistech, paste0(location,"gis_techvrs\\", "ED-",ed_no,"_mics7_vrs_for_gisTech.csv"), row.names = FALSE )
+    write.csv(all_comp_vr_to_process, paste0(location, "ED-",ed_no,"_mics7_vrs.csv"), row.names = FALSE)
+    write.csv(filter_all_comp_vr_to_process, paste0(location, "ED-",ed_no,"_mics7__isBldgLivingQ_vrs.csv"), row.names = FALSE)
+    write.csv(i_hhs3, paste0(location, "ED-",ed_no,"_mics7_hhs_count_by_interview__key.csv"), row.names = FALSE)
+
+    cat(paste0("All mics7 vr building count for ED ",ed_no," is ",nrow(all_comp_vr_to_process),".\n"))
+    cat(paste0("All mics7 vr isbuilding and living quarter count for ED ",ed_no," is ",nrow(filter_all_comp_vr_to_process), ".\n"))
+    cat(paste0("ED ",ed_no," has ", nrow(vr_inconsistencies_filter)," errors! \n"))
+    cat(paste0("All spatial building count ED ", ed_no, " is ", nrow(spat_building_filter), ". \n"))
+    cat(paste0("Total households count for ED ",ed_no," is ",total_hhs, ". \n \n"))
+  }
+
+  location = location
+  cat(paste0("All raw vrs for all EDs are ", nrow(raw_vrs), ".\n"))
+  cat(paste0("All spatial buildings for all EDs are ", nrow(spat_building_process), ".\n"))
+  cat(paste0("All mics_vr buildings for all EDs are ", nrow(vrs), ".\n"))
+  write.csv(vrs, paste0(location, "1. all_mics7_vrs.csv"), row.names = FALSE)
+  write.csv(filter_vrs, paste0(location, "2. all_mics7_filtered_isBldg_LivingQ_vrs.csv"), row.names = FALSE)
+  write.csv(raw_vrs, paste0(location, "0. all_mics7_raw_vrs.csv"), row.names = FALSE)
+  return(print("Completed."))
+}
+
+
+
 
