e6cc566f6ad6ffff65ed3c798ec08b88b3044e89
Rubiere Ramirez <64884499+Rubi3r3@users.noreply.github.com>
2023-12-06 13:14:40 -0600

    realigned the code

--- R/scripts.r
+++ R/scripts.r
@@ -66,28 +66,29 @@ assign_interview__keys<- function (c, e) {
      if ("Orange Walk" %in% fvr$district) {
     fvr$district[fvr$district == "Orange Walk"] <- "2"
     }
-  if ("Belize" %in% fvr$district) {
+    if ("Belize" %in% fvr$district) {
     fvr$district[fvr$district == "Belize"] <- "3"
-  }
-  if ("Corozal" %in% fvr$district) {
-  fvr$district[fvr$district == "Corozal"] <- "1"
-  }
-  if ("Cayo" %in% fvr$district) {
-  fvr$district[fvr$district == "Cayo"] <- "4"
-  }
-   if ("Stann Creek" %in% fvr$district) {
-  fvr$district[fvr$district == "Stann Creek"] <- "5"
-  }
-  if ("Toledo" %in% fvr$district) {
-  fvr$district[fvr$district == "Toledo"] <- "6"
-  }
-  if ("1" %in% fvr$living_quarter) {
-  fvr$living_quarter[fvr$living_quarter == "1"] <- "Yes"
-  }
-  if ("2" %in% fvr$living_quarter) {
-  fvr$living_quarter[fvr$living_quarter == "2"] <- "No"
-  }
-  fvr$blk_uid <- paste0(fvr$district,"-",fvr$ed,"-",fvr$block,"-",fvr$buildingID)
+    }
+    if ("Corozal" %in% fvr$district) {
+    fvr$district[fvr$district == "Corozal"] <- "1"
+    }
+    if ("Cayo" %in% fvr$district) {
+    fvr$district[fvr$district == "Cayo"] <- "4"
+    }
+    if ("Stann Creek" %in% fvr$district) {
+    fvr$district[fvr$district == "Stann Creek"] <- "5"
+    }
+    if ("Toledo" %in% fvr$district) {
+    fvr$district[fvr$district == "Toledo"] <- "6"
+    }
+    if ("1" %in% fvr$living_quarter) {
+    fvr$living_quarter[fvr$living_quarter == "1"] <- "Yes"
+    }
+    if ("2" %in% fvr$living_quarter) {
+    fvr$living_quarter[fvr$living_quarter == "2"] <- "No"
+    }
+
+    fvr$blk_uid <- paste0(fvr$district,"-",fvr$ed,"-",fvr$block,"-",fvr$buildingID)
 
 
      all_comp_vr_raw <- subset(fvr, ed == e_update)
@@ -169,30 +170,31 @@ get_vr <- function(ed_list, location) {
     fvr<- dbGetQuery(db, query_fvr)
 
     if ("Orange Walk" %in% fvr$district) {
-    fvr$district[fvr$district == "Orange Walk"] <- "2"
-    }
-  if ("Belize" %in% fvr$district) {
-    fvr$district[fvr$district == "Belize"] <- "3"
-  }
-  if ("Corozal" %in% fvr$district) {
-  fvr$district[fvr$district == "Corozal"] <- "1"
-  }
-  if ("Cayo" %in% fvr$district) {
-  fvr$district[fvr$district == "Cayo"] <- "4"
-  }
-   if ("Stann Creek" %in% fvr$district) {
-  fvr$district[fvr$district == "Stann Creek"] <- "5"
-  }
-  if ("Toledo" %in% fvr$district) {
-  fvr$district[fvr$district == "Toledo"] <- "6"
-  }
-  if ("1" %in% fvr$living_quarter) {
-  fvr$living_quarter[fvr$living_quarter == "1"] <- "Yes"
-  }
-  if ("2" %in% fvr$living_quarter) {
-  fvr$living_quarter[fvr$living_quarter == "2"] <- "No"
-  }
-  fvr$blk_uid <- paste0(fvr$district,"-",fvr$ed,"-",fvr$block,"-",fvr$buildingID)
+      fvr$district[fvr$district == "Orange Walk"] <- "2"
+      }
+    if ("Belize" %in% fvr$district) {
+      fvr$district[fvr$district == "Belize"] <- "3"
+      }
+    if ("Corozal" %in% fvr$district) {
+      fvr$district[fvr$district == "Corozal"] <- "1"
+      }
+    if ("Cayo" %in% fvr$district) {
+      fvr$district[fvr$district == "Cayo"] <- "4"
+      }
+    if ("Stann Creek" %in% fvr$district) {
+     fvr$district[fvr$district == "Stann Creek"] <- "5"
+      }
+    if ("Toledo" %in% fvr$district) {
+     fvr$district[fvr$district == "Toledo"] <- "6"
+      }
+    if ("1" %in% fvr$living_quarter) {
+     fvr$living_quarter[fvr$living_quarter == "1"] <- "Yes"
+      }
+    if ("2" %in% fvr$living_quarter) {
+      fvr$living_quarter[fvr$living_quarter == "2"] <- "No"
+      }
+   
+   fvr$blk_uid <- paste0(fvr$district,"-",fvr$ed,"-",fvr$block,"-",fvr$buildingID)
     
     all_comp_vr_raw <- subset(fvr, ed == ed_no)
     
