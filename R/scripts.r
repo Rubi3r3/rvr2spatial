a9530988129ae4f620e04228a9c3f7d90af2f7d2
Rubiere Ramirez <64884499+Rubi3r3@users.noreply.github.com>
2023-12-06 13:41:36 -0600

    added license file

--- R/scripts.r
+++ R/scripts.r
@@ -64,34 +64,34 @@ assign_interview__keys<- function (c, e) {
      fvr<- dbGetQuery(db, query_fvr)
 
      if ("Orange Walk" %in% fvr$district) {
-    fvr$district[fvr$district == "Orange Walk"] <- "2"
-    }
-    if ("Belize" %in% fvr$district) {
-    fvr$district[fvr$district == "Belize"] <- "3"
-    }
-    if ("Corozal" %in% fvr$district) {
-    fvr$district[fvr$district == "Corozal"] <- "1"
-    }
-    if ("Cayo" %in% fvr$district) {
-    fvr$district[fvr$district == "Cayo"] <- "4"
-    }
-    if ("Stann Creek" %in% fvr$district) {
-    fvr$district[fvr$district == "Stann Creek"] <- "5"
-    }
-    if ("Toledo" %in% fvr$district) {
-    fvr$district[fvr$district == "Toledo"] <- "6"
-    }
-    if ("1" %in% fvr$living_quarter) {
-    fvr$living_quarter[fvr$living_quarter == "1"] <- "Yes"
-    }
-    if ("2" %in% fvr$living_quarter) {
-    fvr$living_quarter[fvr$living_quarter == "2"] <- "No"
-    }
+      fvr$district[fvr$district == "Orange Walk"] <- "2"
+      }
+     if ("Belize" %in% fvr$district) {
+      fvr$district[fvr$district == "Belize"] <- "3"
+      }
+     if ("Corozal" %in% fvr$district) {
+      fvr$district[fvr$district == "Corozal"] <- "1"
+      }
+     if ("Cayo" %in% fvr$district) {
+      fvr$district[fvr$district == "Cayo"] <- "4"
+      }
+     if ("Stann Creek" %in% fvr$district) {
+      fvr$district[fvr$district == "Stann Creek"] <- "5"
+      }
+     if ("Toledo" %in% fvr$district) {
+      fvr$district[fvr$district == "Toledo"] <- "6"
+      }
+     if ("1" %in% fvr$living_quarter) {
+      fvr$living_quarter[fvr$living_quarter == "1"] <- "Yes"
+      }
+     if ("2" %in% fvr$living_quarter) {
+      fvr$living_quarter[fvr$living_quarter == "2"] <- "No"
+      }
 
     fvr$blk_uid <- paste0(fvr$district,"-",fvr$ed,"-",fvr$block,"-",fvr$buildingID)
 
 
-     all_comp_vr_raw <- subset(fvr, ed == e_update)
+    all_comp_vr_raw <- subset(fvr, ed == e_update)
 
     all_comp_vr_to_process <- dplyr::distinct(all_comp_vr_raw, interview__key, .keep_all = TRUE)
     all_comp_vr_to_process <- dplyr::filter(all_comp_vr_to_process, responsible1.x != 'DELETION_VR23')
