da7a8e4d630d8148d51c8a13a27fb1eea2431e10
Rubiere Ramirez <64884499+Rubi3r3@users.noreply.github.com>
2023-12-07 08:30:08 -0600

    fixed the clear_ed code

--- R/scripts.r
+++ R/scripts.r
@@ -15,8 +15,9 @@
 clear_ed<- function(c, e, t) {
     conn2 <- c
     t <- t
+    e <- e
     for (s in 1:length(e)) {
-      e <- e [[s]]
+      ed_no <- e[[s]]
       query<- paste0("UPDATE ",t," SET photovalid = NULL,
       photo_cnt = NULL, living_quarter = NULL, bldg_refn = bldg_number,
       bldg_newn = NULL,
@@ -24,7 +25,7 @@ clear_ed<- function(c, e, t) {
       population = NULL,
       tot_hh_number = NULL,
       interview__key = NULL,
-      int_bldg_desc = NULL WHERE ed_2023 = '", e ,"'; ")
+      int_bldg_desc = NULL WHERE ed_2023 = '", ed_no ,"'; ")
  
       DBI::dbExecute(conn2, query)
         
